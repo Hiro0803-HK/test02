52a0f877b191ef819712b2556b2d8c4fa64a506c
Hiro0803-HK <kd1339439@st.kobedenshi.ac.jp>
2022-12-19 16:52:11 +0900

    aa

--- testst03_1.py
+++ testst03_1.py
@@ -8,18 +8,15 @@ import streamlit as st
 #import numpy as np
 #import pandas as pd
 from PIL import Image
-#import time
-
-st.title('グループ制作サンプル')
+import time
+st.title('マイクロゼット')
 
 PAGE = st.sidebar.selectbox(
     "ページ選択", ["ページ1", "ページ2","ページ3"], 
     key="page-select"
     )
 
-YBCList = ["瘦せ型", "普通", "ふくよか"] 
-
-colp1_01, colp1_02, colp1_03 = st.columns(3)
+YBCList = ["瘦せ型　　　　　　　　　　", "普通　　　　　　　　　　", "がっちり"] 
 
 cont = st.container()
 cont2 = st.container()
@@ -30,13 +27,14 @@ colp2_01, colp2_02, colp2_03 = cont.columns([0.1, 6, 0.1])
 colp1_01, colp1_02, colp1_03 = cont2.columns(3)
 colp4_01, colp4_02, colp4_03 = cont3.columns([0.1, 6, 0.1])
 colp3_01, colp3_02, colp3_03 = cont4.columns(3)
+pg3_col01, pg3_col02, pg3_col03 = st.columns(3)
 
 Col01 = [colp1_01, colp1_02, colp1_03]
 Col02 = [colp3_01, colp3_02, colp3_03]
 
 TList = ["やや低め", "平均程度", "高め"]
 
-IMList = ["清楚", "クール", "明るめ", "大人", "モード"]
+IMList = ["清楚", "クール", "明るめ", "大人","渋い"]
 
 YSize = ["とてもぴったり", "ぴったり", "少しぴったり", "普通", "少しゆったり", "ゆったり", "とてもゆったり"]
 
@@ -72,22 +70,47 @@ def page1():
             
             st.session_state["page-select"] = "ページ2"
     
-
-    
        st.slider("あなたの身長は？", 100, 200, 150, key="ytl")
+       st.write("\n")
+
+       st.write("あなたの体系は？")
+       col1,col2,col3 = st.columns(3)     
+       with col1:
+           image = Image.open('ほっそり.png')
+           st.image(image,width=70)
+           
+       with col2:
+           image = Image.open('標準.png')
+           st.image(image,width=82)
+     
+       with col3:
+           image = Image.open('がっちり.png')
+           st.image(image,width=100)
+
         
-       st.radio("あなたの体型は", YBCList, key="ybc")
-        
+           
+       
+ 
+           
+       st.radio("　", YBCList, key="ybc",horizontal=True)
+       st.write("\n")
+ 
+       
        st.radio("あなたの目指す雰囲気は？", IMList, key="yhs", horizontal=True)
+       st.write("\n")
         
-       st.write("ここに画像挿入")
+      # st.write("ここに画像挿入")
 
-       st.select_slider("あなたの着てみたいサイズ感は", YSize, YSize[3], key="ysz")  
-
-       st.selectbox("あなたの好きなマスクの色は", YMsk, key="Ymk")
+       st.select_slider("あなたの着たいサイズ感は？", YSize, YSize[3], key="ysz")
+       st.write("\n")
+       
+       
+       st.selectbox("あなたの好きなマスクの色は？", YMsk, key="Ymk")
+       st.write("\n")
+            
       
+        
        st.button(label = "決定", on_click = change_page)
-
       
 def page2():
         
@@ -105,6 +128,7 @@ def page2():
      with colp2_02:
 
             st.multiselect("あなたの好きな上の服は", wear01, key="Lwear01")
+
             YLW = st.session_state["Lwear01"]
 
      with colp2_03:
@@ -158,15 +182,28 @@ def page2():
 def page3():
     
     def change_page():
+                
+          st.session_state["page-select"] = "ページ1"
         
-        st.session_state["page-select"] = "ページ1"
+    image = Image.open('IMG_7837 (2).PNG')
+
+    with pg3_col01:
+            st.write("あなたに似合う服の系統は")
+
+    with pg3_col02:
+            st.empty()  
+
+
+    with pg3_col03:
+            st.header("ストリート")
+            st.image(image, caption='IMG_7837 (2).PNG',use_column_width=True)
+
         
-
-    st.write("結果表示")
-
-
-
-
+         
+         
+         
+    st.button(label = "戻る", on_click = change_page)
+    
 
 if PAGE == "ページ1":
     page1()
@@ -176,5 +213,4 @@ elif PAGE == "ページ2":
     
 elif PAGE == "ページ3":
     page3()
-
 
